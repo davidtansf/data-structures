115c161c8ad1be9380a7c333c2260f69155e2c1e
David Tan <davidtansf@gmail.com>
2015-08-20 17:16:54 -0700

    [refactor] refactor pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,29 @@
+var Queue = function(){
+  this._storage = {};
+  this._start = 0;
+  this._end = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this._storage[this._end] = value;
+  this._end++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this._start < this._end) {
+    var temp = this._storage[this._start];
+    delete this._storage[this._start];
+    this._start++;
+    return temp;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this._end - this._start;
+};
+
+/*
+
 var Queue = function() {
 	this.getSize = 0;
 	this.storage = {};
@@ -29,4 +55,4 @@ Queue.prototype.size = function() {
 	return this.getSize;
 }
 
-var Starbucks = new Queue();
+*/
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,22 +1,22 @@
-var Stack = function () {
-	this.getSize = 0;
-	this.storage = {};
+var Stack = function() {
+  this._storage = {};
+  this._size = 0;
 };
 
-Stack.prototype.push = function(value) {
-	this.storage[this.getSize] = value;
-	this.getSize++;
+Stack.prototype.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
 };
 
-Stack.prototype.pop = function() {
-  if (this.getSize > 0) {
-		this.getSize--;
-		var item = this.storage[this.getSize];
-		delete this.storage[this.getSize];
-		return item;
-	}
+Stack.prototype.pop = function(){
+  if(this._size) {
+    this._size--;
+    var temp = this._storage[this._size];
+    delete this._storage[this._size];
+    return temp;
+  }
 };
 
-Stack.prototype.size = function() {
-	return this.getSize;
+Stack.prototype.size = function(){
+  return this._size;
 };
