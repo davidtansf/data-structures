995c7e21720f6dea6031bfcc8a2047fb9d12b0c1
David Tan <davidtansf@gmail.com>
2015-08-21 15:57:31 -0700

    [refactor] update solution for Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,60 +1,20 @@
-// var Set = function(){
-  
-//   var set = Object.create(setPrototype);
-//   set.storage = []; // solution set, create object instead
-//   return set;
-
-// };
-
-// var setPrototype = {
-
-// 	add: function(item) {
-//     var found = 0;
-//     for (var i = 0; i < this.storage.length; i++) {
-//     	if (item === this.storage[i]) {
-//     		found = 1;
-//     		break;
-//     	}
-//     }	
-//     if (!found || this.storage.length === 0) {
-//     	this.storage.push(item);
-//     }
-// 	},
-    
-// 	contains: function(item) {
-//     for (var i = 0; i < this.storage.length; i++) {
-//       if (item === this.storage[i]) {
-//       	 return true;
-//       }  
-// 	  }
-// 	  return false;
-// 	},  
-
-// 	remove: function(item) {
-//     for (var i = 0; i < this.storage.length; i++) {
-//       if (item === this.storage[i]) {
-//       	 delete this.storage[i];
-//       }  
-// 	  }
-// 	}
-// };
-
-var Set = function () {
+var Set = function(){
   var set = Object.create(setPrototype);
-  set.storage = {};
+  set._storage = {};
   return set;
 };
 
-var setPrototype = {};
+var setPrototype = {
 
-setPrototype.add = function(value) {
-  return this.storage[value] = value;
-};
+  add: function(item) {
+    this._storage[item] = true;
+  },
 
-setPrototype.contains = function(value) {
-  return !!this.storage[value];
-};
+  contains: function(item) {
+    return !!this._storage[item];
+  },
 
-setPrototype.remove = function(value) {
-  delete this.storage[value];
+  remove: function(item) {
+    delete this._storage[item];
+  }
 };
