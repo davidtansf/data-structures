21ac36d4eff4c6fa1156d8d930551956081a3abd
David Tan <davidtansf@gmail.com>
2015-08-20 17:13:44 -0700

    [refactor] refactor prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,3 +1,36 @@
+var Queue = function(){
+
+  var someInstance = Object.create(queueMethods);
+
+  someInstance._storage = {};
+  someInstance._start = 0;
+  someInstance._end = 0;
+
+  return someInstance;
+};
+
+var queueMethods = {
+
+  enqueue: function(value){
+    this._storage[this._end] = value;
+    this._end++;
+  },
+
+  dequeue: function(){
+    if (this._start < this._end) {
+      var temp = this._storage[this._start];
+      delete this._storage[this._start];
+      this._start++;
+      return temp;
+    }
+  },
+
+  size: function(){
+    return this._end - this._start;
+  }
+};
+
+/*
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
@@ -41,5 +74,4 @@ var queueMethods = {
 }
 
 };
-
-var test = Queue();
+*/
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,26 +1,62 @@
 var Stack = function() {
-	var newStack = Object.create(stackMethods);
-	newStack.getsize = 0;
-	newStack.storage = {};
-	return newStack;
+
+  var someInstance = Object.create(stackMethods);
+  someInstance._storage = {};
+  someInstance._size = 0;
+
+  return someInstance;
 };
 
-stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this._storage[this._size] = value;
+    this._size++;
+  },
 
-stackMethods.push = function(value) {
-	this.storage[this.getsize] = value;
-	this.getsize++;
+  pop: function(){
+    if(this._size) {
+      this._size--;
+      var temp = this._storage[this._size];
+      delete this._storage[this._size];
+      return temp;
+    }
+  },
+
+  size: function(){
+    return this._size;
+  }
 };
 
-stackMethods.pop = function() {
-	if (this.getsize > 0) {
-		this.getsize--;
-		var temp = this.storage[this.getsize];
-		delete this.storage[this.getsize];
-		return temp;
-	}
+/* // preferred way (but doesn't pass test)
+// methods stored as property of Stack function, instead of creating a new global variable
+
+var Stack = function() {
+
+  var someInstance = Object.create(Stack.methods);
+  someInstance._storage = {};
+  someInstance._size = 0;
+
+  return someInstance;
 };
 
-stackMethods.size = function() {
-	return this.getsize;
+Stack.methods = {
+  push: function(value){
+    this._storage[this._size] = value;
+    this._size++;
+  },
+
+  pop: function(){
+    if(this._size) {
+      this._size--;
+      var temp = this._storage[this._size];
+      delete this._storage[this._size];
+      return temp;
+    }
+  },
+
+  size: function(){
+    return this._size;
+  }
 };
+
+*/
